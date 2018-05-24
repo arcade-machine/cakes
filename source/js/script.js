8ca06f5cc7ed13173807f4ba57ede7c0d5adcde0
Сергей <agrael@yandex.ru>
2018-05-24 15:59:40 +0300

    Завершение мобильной верстки. Начало планшетной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,11 @@
 var menuOpen = document.querySelector(".header__burger");
 var menu = document.querySelector(".main-navigation");
 var nojs = document.querySelector(".header");
+var sliderOne = document.querySelector(".size__item--1kg");
+var sliderTwo = document.querySelector(".size__item--2kg");
+var oneKilo = document.getElementById("1kg");
+var twoKilo = document.getElementById("2kg");
+
 
 nojs.classList.remove("header__no-js");
 
@@ -13,3 +18,19 @@ menuOpen.addEventListener("click", function(evt) {
         menu.classList.remove("main-navigation__open");
     }
 });
+
+// oneKilo.addEventListener("change", function() {
+//     if (oneKilo.checked) {
+//         sliderOne.classList.add("size__item--current");
+//     } else {
+//         sliderOne.classList.remove("size__item--current");
+//     }
+// });
+//
+// twoKilo.addEventListener("change", function() {
+//     if (twoKilo.checked) {
+//         sliderTwo.classList.add("size__item--current");
+//     } else {
+//         sliderTwo.classList.remove("size__item--current");
+//     }
+// });
